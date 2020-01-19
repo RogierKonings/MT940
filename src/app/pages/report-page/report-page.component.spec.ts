8a264f81a9fd23e3502ce34633bfdf4583947ab1
Rogier Konings <Rogier_konings@hotmail.com>
2020-01-19 19:27:21 +0100

    test warning message

--- src/app/pages/report-page/report-page.component.spec.ts
+++ src/app/pages/report-page/report-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ReportPageComponent } from 'src/app/pages/report-page/report-page.component';
 import { ReportService } from 'src/app/core/services/report.service';
@@ -37,6 +38,19 @@ describe('ReportPageComponent', () => {
       spyOn(reportService, 'createReport').and.returnValue({});
       component.openFile(fakeFile);
       expect(reportService.createReport).toHaveBeenCalled();
+    });
+
+    it('should show an error message when the report service returns an error', () => {
+      const blob = new Blob([''], { type: 'text/csv'});
+      const fakeFile = blob as File;
+
+      spyOn(reportService, 'createReport').and.throwError('something went wrong!');
+
+      component.openFile(fakeFile);
+      fixture.detectChanges();
+      const warning = fixture.debugElement.query(By.css('.alert-danger')).nativeElement;
+
+      expect(warning).toBeTruthy();
     });
   });
 
